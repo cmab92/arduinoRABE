9a03b712dbf19cc9e053ba75fe885ada8e7c9526
cmab92 <christopher.bonenberger@googlemail.com>
2018-03-26 14:03:25 +0200

    ...

--- hcsr04/src/main.cpp
+++ hcsr04/src/main.cpp
@@ -3,134 +3,104 @@
  * Author: Bonenberger
  * Purpose: trigger, readout and publish multiple HCSR04 USS
  * Update: all in one msg
+ todo EQUIDISTANT SAMPLING
  */
 #include <ros.h>
 #include <std_msgs/Float32.h>
+#include <TimerOne.h>
 
-// defines pins numbers
-#define TRIGGER1 6
-#define TRIGGER2 7
-#define TRIGGER3 8
-#define ECHO1 10
-#define ECHO2 11
-#define ECHO3 12
+#define TRIGGER0 6
+#define TRIGGER1 7
+#define TRIGGER2 8
+#define ECHO0 10
+#define ECHO1 11
+#define ECHO2 12
+#define TSAMPLE 16500 //in us (16500us := measuring cycle of 0a41sk )
 #define BAUDRATE 57600
+
+double distance[3] = {0, 0, 0};
+
 // ros
 ros::NodeHandle nh;
+std_msgs::Float32 distance_msg0;
 std_msgs::Float32 distance_msg1;
 std_msgs::Float32 distance_msg2;
-std_msgs::Float32 distance_msg3;
-ros::Publisher pub_dist2("pub_dist1", &distance_msg1);
-ros::Publisher pub_dist1("pub_dist2", &distance_msg2);
-ros::Publisher pub_dist3("pub_dist3", &distance_msg3);
+ros::Publisher pub_dist0("pub_dist0", &distance_msg0);
+ros::Publisher pub_dist1("pub_dist1", &distance_msg1);
+ros::Publisher pub_dist2("pub_dist2", &distance_msg2);
+
+void measure( void ) {
+  for (int i = 0; i < 3; i++) {
+    if ( (distance[i] < 2.0)&&(distance[i] > 0) ) { distance[i] = distance[i]; }
+    else { distance[i] = 4; }
+  }
+  // send data
+  distance_msg0.data = distance[0];
+  pub_dist0.publish( &distance_msg0 );
+  distance_msg1.data = distance[1];
+  pub_dist1.publish( &distance_msg1 );
+  distance_msg2.data = distance[2];
+  pub_dist2.publish( &distance_msg2 );
+  nh.spinOnce();
+}
 
 void setup() {
-  pinMode(TRIGGER1, OUTPUT); // Sets the trigPin as an Output
-  pinMode(TRIGGER2, OUTPUT); // Sets the trigPin as an Output
-  pinMode(TRIGGER3, OUTPUT); // Sets the trigPin as an Output
-  pinMode(ECHO1, INPUT); // Sets the echoPin as an Input
-  pinMode(ECHO2, INPUT); // Sets the echoPin as an Input
-  pinMode(ECHO3, INPUT); // Sets the echoPin as an Input
-  Serial.begin(BAUDRATE); // Starts the serial communication
-  Serial.println("setup");
+  // Serial.begin(BAUDRATE);
+
+  pinMode(TRIGGER0, OUTPUT);
+  pinMode(TRIGGER1, OUTPUT);
+  pinMode(TRIGGER2, OUTPUT);
+  pinMode(ECHO0, INPUT);
+  pinMode(ECHO1, INPUT);
+  pinMode(ECHO2, INPUT);
+  digitalWrite(TRIGGER0, LOW);
   digitalWrite(TRIGGER1, LOW);
   digitalWrite(TRIGGER2, LOW);
-  digitalWrite(TRIGGER3, LOW);
   nh.initNode();
+  nh.advertise(pub_dist0);
   nh.advertise(pub_dist1);
   nh.advertise(pub_dist2);
-  nh.advertise(pub_dist3);
 
-  Serial.println("setup");
+  Timer1.initialize(TSAMPLE);
+  Timer1.attachInterrupt(measure);
 }
 
 void loop() {
   unsigned long startTime = micros();
-  unsigned long startTime1 = micros();
-  unsigned long startTime2 = micros();
-  unsigned long startTime3 = micros();
+  unsigned long sensorStartTime[3] = {0, 0, 0};
   unsigned long passedTime;
-  unsigned long sensorTime1 = 0;
-  unsigned long sensorTime2 = 0;
-  unsigned long sensorTime3 = 0;
-  double distance1;
-  double distance2;
-  double distance3;
-  bool echoDet1;
-  bool echoDet2;
-  bool echoDet3;
-  unsigned int sensorCnt = 5;
-
-  while(1){
+  unsigned long sensorTime[3] = {0, 0, 0};
+  int echoDet[3] = {digitalRead(ECHO0), digitalRead(ECHO1), digitalRead(ECHO2)};
+  while(1) {
     passedTime = micros() - startTime;
-    echoDet1 = digitalRead(ECHO1);
-    echoDet2 = digitalRead(ECHO2);
-    echoDet3 = digitalRead(ECHO3);
-
-    if ( (passedTime > 10000) || (passedTime < 0) ) {
+    echoDet[0] = digitalRead(ECHO0);
+    echoDet[1] = digitalRead(ECHO1);
+    echoDet[2] = digitalRead(ECHO2);
+    if ( (passedTime > 10000) || (passedTime < 0) ) {    // detects targets in ~1.70m distance
       startTime = micros();
-      digitalWrite(sensorCnt, HIGH);
+      digitalWrite(TRIGGER0, HIGH);
+      digitalWrite(TRIGGER1, HIGH);
+      digitalWrite(TRIGGER2, HIGH);
       delayMicroseconds(10);
-      digitalWrite(sensorCnt, LOW);
-      if (sensorCnt > 8){
-        sensorCnt = 6;
-      }
-      else {
-        sensorCnt += 1;
-      }
-      //
-      if ( (distance1 < 2.0)&&(distance1 > 0) ) {
-        distance1 = distance1;
-      }
-      else {
-        distance1 = 4;
-      }
-      distance_msg1.data = distance1;
-      pub_dist1.publish( &distance_msg1 );
-      //
-      if ( (distance2 < 2.0)&&(distance2 > 0) ) {
-        distance2 = distance2;
-      }
-      else {
-        distance2 = 4;
-      }
-      distance_msg2.data = distance2;
-      pub_dist2.publish( &distance_msg2 );
-      //
-      if ( (distance3 < 2.0)&&(distance3 > 0) ) {
-        distance3 = distance3;
-      }
-      else {
-        distance3 = 4;
-      }
-      distance_msg3.data = distance3;
-      pub_dist3.publish( &distance_msg3 );
-      nh.spinOnce();
+      digitalWrite(TRIGGER0, LOW);
+      digitalWrite(TRIGGER1, LOW);
+      digitalWrite(TRIGGER2, LOW);
     }
     else {
-      if ( digitalRead(ECHO1) == 1 ) {
-        sensorTime1 = micros() - startTime1;
-      }
-      if ( digitalRead(ECHO2) == 1 ) {
-        sensorTime2 = micros() - startTime2;
-      }
-      if ( digitalRead(ECHO3) == 1 ) {
-        sensorTime3 = micros() - startTime3;
-      }
-      if ( (digitalRead(ECHO1) != echoDet1) && (echoDet1 == 1) ) {
-        distance1 = (float)sensorTime1 * 0.0001724;
-      }
-      if ( (digitalRead(ECHO2) != echoDet2) && (echoDet2 == 1) ) {
-        distance2 = (float)sensorTime2 * 0.0001724;
-      }
-      if ( (digitalRead(ECHO3) != echoDet3) && (echoDet3 == 1) ) {
-        distance3 = (float)sensorTime3 * 0.0001724;
+      for (int i = 0; i<3; i++) {
+        // update sensorTime
+        if ( digitalRead( (10+i) ) == 1 ) {
+          sensorTime[i] = micros() - sensorStartTime[i];
+        }
+        // check for falling edge of echo ... calculate distance in m
+        if ( (digitalRead( (10+i) ) != echoDet[i]) && (echoDet[i] == 1) ) {
+          distance[i] = (float)sensorTime[i] * 0.0001724;
+        }
       }
     }
-
-    if ( (digitalRead(ECHO1) != echoDet1) && (echoDet1 == 0) ) { startTime1 = micros(); }
-    if ( (digitalRead(ECHO2) != echoDet2) && (echoDet2 == 0) ) { startTime2 = micros(); }
-    if ( (digitalRead(ECHO3) != echoDet3) && (echoDet3 == 0) ) { startTime3 = micros(); }
-
+    // check for rising edge of echo
+    for (int i = 0; i<3; i++) {
+      if ( (digitalRead( (10+i) ) != echoDet[i]) && (echoDet[i] == 0) ) { sensorStartTime[i] = micros(); }
+    }
   }
 }
